8b1b5ee8f5d36cd5f177d42dd18891a1d0feda96
Curtis Thomas <83663235+fuzzyduckers@users.noreply.github.com>
2022-11-15 14:07:03 +0200

    Upgrade Game Test

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+"use-strict";
 
+let myHealth = 100;
+let myAttack = 50;
+let enemyHealth = 100;
+
+let enemyAttack = 1;
+
+let enemyNo = 1;
+
+setInterval(function updateMyGame() {
+  document.querySelector(".healthNumber").textContent = myHealth;
+  document.querySelector(".healthBar").style.width = myHealth + "%";
+
+  if (enemyHealth >= 0) {
+    document.querySelector(".enemyHealthNumber").textContent = enemyHealth;
+    document.querySelector(".enemyHealthBar").style.width = enemyHealth + "%";
+  } else {
+    document.querySelector(".enemyNo").textContent = enemyNo++;
+    enemyHealth = enemyNo * 100;
+  }
+  // <insert highly sophisticated money += 1 code here>
+}, 1000);
+
+document.querySelector(".btnAttack").addEventListener("click", function () {
+  enemyHealth -= myAttack;
+  document.querySelector(".enemyHealthNumber").textContent = enemyHealth;
+});
+
+document.querySelector(".fight").addEventListener("click", function () {
+  document.querySelector(".fight").style.visibility = "hidden";
+  setInterval(function enemyAttacks() {
+    myHealth -= enemyAttack;
+  }, 1000);
+});
+
+document
+  .querySelector(".btnAutoUpgrade")
+  .addEventListener("click", function () {
+    setInterval(function enemyAttacks() {
+      enemyHealth -= myAttack;
+    }, 1000);
+  });
